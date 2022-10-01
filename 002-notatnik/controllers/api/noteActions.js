880ee197a368fac09ab4191ed2d95f354e488a58
elhispanopolako <tadeo98hiszpan@gmail.com>
2022-10-01 16:42:47 +0200

    Testy w postmanie

--- 002-notatnik/controllers/api/noteActions.js
+++ 002-notatnik/controllers/api/noteActions.js
@@ -1,27 +1,33 @@
 const Note = require('../../db/models/note');
 class NoteActions {
     saveNotes(req, res) {
-        const newNote = new Note({
-            title: 'Zrobić zakupy',
-            body: 'mleko,jaja,woda'
-        });
-        newNote.save().then(() => {
-            console.log('Notatka została zapisana')
-        });
+        /*   const newNote = new Note({
+               title: 'Zrobić zakupy',
+               body: 'mleko,jaja,woda'
+           });
+           newNote.save().then(() => {
+               console.log('Notatka została zapisana')
+           });*/
+        const title = req.body.title
+        const body = req.body.body;
 
-        res.send('Strona główna działa')
+        res.send('Notatka została zapisana z body:' + body + " i title:" + title)
     }
     getAllNotes(req, res) {
-
+        res.send('getAllNotes działa')
     }
     getNote(req, res) {
+        const id = req.params.id;
+        res.send('getNote działa i podaje mi id = ' + id)
 
     }
     updateNote(req, res) {
-
+        const id = req.params.id;
+        res.send('updateNote działa i aktualizuję notatke o id = ' + id)
     }
     deleteNote(req, res) {
-
+        const id = req.params.id;
+        res.send('deleteNote działa i usuwa notatke o id = ' + id)
     }
 }
 module.exports = new NoteActions();
